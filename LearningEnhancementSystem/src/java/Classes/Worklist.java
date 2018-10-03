93f05313dc920bb7763c213454979a47ff29aef2
Espen <espeno17@uia.no>
2018-10-03 17:08:40 +0200

    Oppdatert
    Lagt litt inn i Worklist servletten samt skiftet på Worklist java klasssen. Ikke bry oss  med 2 Array lister for nå. Prøv å få til at den skiver ut alle deliveravbels.

--- LearningEnhancementSystem/src/java/Classes/Worklist.java
+++ LearningEnhancementSystem/src/java/Classes/Worklist.java
@@ -11,13 +11,49 @@ import java.util.ArrayList;
  *
  * @author Kivi_
  */
-public class Worklist {
+//public class Worklist {
     
-    private int worklistid;
-    private int teacherid;
+  //  private int worklistid;
+    //private int teacherid;
     
-    private ArrayList<Evaluation> Todo = new ArrayList<>();
-    private ArrayList<Evaluation> Evaluated = new ArrayList<>();
+   // private ArrayList<Evaluation> Todo = new ArrayList<>();
+   // private ArrayList<Evaluation> Evaluated = new ArrayList<>();
    
-}
+public class Worklist {
+        
+    private int id;
+    private int student_id;
+    private int module_id;
+    private String delivery_content;
+    private int worklist_id;
+    private int delivery_timestamp; 
+    private ArrayList<Worklist> worklist = new ArrayList<>();
 
+    public int getId() {
+        return id;
+    }
+
+    public int getStudent_id() {
+        return student_id;
+    }
+    
+    public int module_id() {
+        return module_id;
+    }
+    
+    public String delivery_content() {
+        return delivery_content;
+    }
+    
+    public int worklist_id() {
+           return worklist_id;
+    }
+    
+    public int delivery_timestamp() {
+        return delivery_timestamp;
+    }
+    
+    public ArrayList<Worklist> getWorklist(){
+        return worklist;
+    }
+    }
--- LearningEnhancementSystem/src/java/Database/WorklistDb.java
+++ LearningEnhancementSystem/src/java/Database/WorklistDb.java
@@ -5,6 +5,7 @@
  */
 package Database;
 
+import Classes.Evaluation;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -27,28 +28,27 @@ public class WorklistDb extends Database {
     private static final String SLCT_EVALUATION_WITH_DELIVERY = "select e.evaluation_id, u.teacher_id, d.delivary_id, e.evaluation_comment, e.evaluation_isPublished = ?";
     private static final String SLCT_EVALUATION = "select * from Evaluation where evalution_isPublished = false";
     /**
-     * This method retrieves all of the modules in the database, create an object of each record and is then
-     * added to a list of modules
-     * @return a list of module-objects.
+     * This method retrieves all of the deliveries in the database, create an object of each record and is then
+     * added to a list of deliveries
+     * @return a list of delivery-objects.
      */
-    public List<Evaluation> getEvaluation(){
-        List<Evaluation> evaluation = new ArrayList<>();
+    public List<Worklist> getDelivery(){
+        List<Worklist> worklist = new ArrayList<>();
         
         try (
             Connection conn = getConnection();
             Statement stmt = getStatement(conn);
-            ResultSet modulSet = stmt.executeQuery(SLCT_ALL_EVALUATIONS);
+            DeliverySet deliverySet = stmt.executeQuery(SLCT_ALL_DELIVERY);
           ){
-            while(modulSet.next()) {
-                Module modul = new Module();
-                modul.setId(modulSet.getInt("module_id"));
-                modul.setName(modulSet.getString("module_name"));
-                modul.setContent(modulSet.getString("module_content"));
-                modul.setDesc(modulSet.getString("module_desc"));
-                modul.setPublished(modulSet.getBoolean("module_isPublished"));
-                moduler.add(modul);
+            while(DeliverySet.next()) {
+                Delivery delivery = new Evaluation();
+                delivery.setId(deliverySet.getInt("delivery_id"));
+                delivery.setStudentName(deliverySet.getString("student_id"));
+                delivery.setDelivaryContent(deliverySet.getString("delivery_content"));
+                delivery.setDesc(deliverySet.getString("delivery_timestamp"));
+                
             }
-            return moduler;
+            return Delivery;
         }
         catch (SQLException ex) {
             System.out.println("Query error:" + ex);
