2aeeb3cccc321268cb108d445e96648d685a032f
Kevin <kevin@smordal.me>
2018-10-03 13:34:36 +0200

    Lagt til Worklist.java

---
+++ LearningEnhancementSystem/src/java/Classes/Worklist.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Kivi_
+ */
+public class Worklist {
+    
+    private int worklistid;
+    private int teacherid;
+    private boolean isPublished;
+    private ArrayList<Evaluation> Todo = new ArrayList<>();
+   
+    
+}
