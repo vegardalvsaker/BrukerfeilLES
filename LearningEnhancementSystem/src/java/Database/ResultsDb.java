0c1208a4cabd6b22932b7b547f7fa6f664d96dc9
gorm <gormerik96@gmail.com>
2018-10-24 11:54:34 +0200

    Opprettet ny branch
    Satt opp relevante klasser

---
+++ LearningEnhancementSystem/src/java/Database/ResultsDb.java
@@ -0,0 +1,32 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import Classes.User;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+/**
+ *
+ * @author Gorm-Erik
+ */
+public class ResultsDb extends Database {
+    
+    
+    
+    
+    public results printResults()   {
+        
+    Results<results> results = new ArrayList<>();
+  
+    try {
+        
+        
+        
+    }
+    
+    }
+    
+    
+}
---
+++ LearningEnhancementSystem/src/java/Servlets/Results.java
@@ -0,0 +1,100 @@
+package Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Gorm-Erik
+ */
+@WebServlet(urlPatterns = {"/Results"})
+public class Results extends HttpServlet {
+
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+            
+            
+            results(out);
+            
+            
+        }
+        
+        
+        
+    }
+
+    private void results(PrintWriter out, HttpServletRequest request)    {
+            
+        out.println("<h1>Resultater:</h1><br>");
+        out.println("");
+        
+        
+        
+        
+    }
+    private void progress(PrintWriter out, HttpServletRequest request)    {
+        
+        out.println("<h1>Fremgang:</h1><br>");
+        
+    }
+    
+        
+            
+            
+                    
+            
+            
+        }
+
+
+
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
