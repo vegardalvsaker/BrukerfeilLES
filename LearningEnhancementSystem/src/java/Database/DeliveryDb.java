e641e1e5fa6282398b19e585a9395079d4a5f853
Filip Østrem <Filipostrem01@gmail.com>
2018-11-11 21:39:11 +0100

    begynt på Edit Delivery

--- LearningEnhancementSystem/src/java/Database/DeliveryDb.java
+++ LearningEnhancementSystem/src/java/Database/DeliveryDb.java
@@ -17,14 +17,9 @@ import java.util.ArrayList;
  * @author Filip
  */
 public class DeliveryDb extends Database{
-<<<<<<< HEAD
 
     private static final String SELECT_DELIVERY = "select * from Delivery where (student_id, module_id) = (?,?)";
     private static final String SELECT_DELIVERY_WITH_USER_NAME = "select u.user_name, d.delivery_id, d.delivery_timestamp, d.student_id, d.module_id,  d.worklist_id, d.delivery_content from Delivery d inner join Users u on d.student_id = u.user_id where d.delivery_id = ?";
-=======
-    
-
->>>>>>> feature_delivery
     private static final String ADD_DELIVERY = "insert into Delivery values (default, ?, ?, ?, ?, default, default)";
     private static final String GET_DELIVERY_FORM ="select * from Module where module_id = ?";
     private static final String SLCT_ALL_DELIVERIES = "select * from Delivery where module_id = ?";
@@ -117,10 +112,7 @@ public class DeliveryDb extends Database{
         catch (SQLException ex) {
             System.out.println("Query error:" + ex);
         }
-<<<<<<< HEAD
     }
-=======
->>>>>>> feature_delivery
     public ArrayList<Delivery> getDelivery(PrintWriter out, String studentID) {
         
         String deliveries = "select d.delivery_id, d.delivery_content, d.delivery_timestamp, d.delivery_isEvaluated, w.worklist_id, s.user_id, m.module_id, m.module_name\n" +
@@ -170,11 +162,7 @@ public class DeliveryDb extends Database{
 
         return null;
     
-<<<<<<< HEAD
     }
-=======
-
->>>>>>> feature_delivery
     public void addDelivery(String studentid, String moduleid, String content, String worklistid) {
         try( Connection conn = getConnection();
              PreparedStatement ps = conn.prepareStatement(ADD_DELIVERY);
@@ -191,9 +179,8 @@ public class DeliveryDb extends Database{
             System.out.println(ex);
         }
     }
-<<<<<<< HEAD
     
-    public boolean editModule (String moduleId, String content){
+    public boolean editDelivery (String moduleId, String content){
         String moduleId = request.getParameter("id");
         String editDeliveryContent = "update Delivery set delivery_content = ?, where delivery_id = ?";
         
@@ -202,7 +189,7 @@ public class DeliveryDb extends Database{
             PreparedStatement prepStatement = connection.prepareStatement(editDeliveryContent);
         
         ) {
-            prepStatement.setString(1, content);
+            prepStatement.setString(1, id);
             prepStatement.setString(1, content);
         
             prepStatement.executeUpdate();
@@ -265,8 +252,6 @@ public class DeliveryDb extends Database{
     //^^^^ FOSSE ^^^^
     
     
-=======
->>>>>>> feature_delivery
 }
  
      
--- LearningEnhancementSystem/src/java/Servlets/EditDelivery.java
+++ LearningEnhancementSystem/src/java/Servlets/EditDelivery.java
@@ -5,10 +5,40 @@
  */
 package Servlets;
 
+import Database.DeliveryDb;
+import HtmlTemplates.BootstrapTemplate;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+
 /**
  *
- * @author Frozk
+ * @author Filip
  */
-public class EditDelivery {
+@WebServlet (name ="EditDelivery", urlPatterns = {"/EditDelivery"})
+public class EditDelivery extends SuperServlet {
+    BootstrapTemplate bootstrap = new BootstrapTemplate();
+    
+    protect void processRequest (httpServletRequest request, HttpServletRespons respons)
+            throws ServletException, IOException {
+        respons-setContentType();
+        
+        try (PrintWriter out = response.getWriter()) {
+            
+            super.processRequest (request, response, "Delivery", out);
+            DeliveryDb db = new DeliveryDb();
+            db.init();
+            
+            if (request.getMethod().equls("Edit")) {
+                
+                String deliveryContent = request.getParameter ("Link");
+                
+                db.editModule(out, request, deliveryContent);
+            }
+        } 
+               
+    }
+    
     
 }
