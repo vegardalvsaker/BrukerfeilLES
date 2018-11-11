c688b4f903b6ade439ff744164070b4823d8c9b0
Filip Østrem <Filipostrem01@gmail.com>
2018-11-11 18:00:48 +0100

    edit delivery progress

--- LearningEnhancementSystem/src/java/Database/DeliveryDb.java
+++ LearningEnhancementSystem/src/java/Database/DeliveryDb.java
@@ -3,6 +3,8 @@ package Database;
 
 import java.sql.*;
 import Classes.Delivery;
+import static com.sun.corba.se.spi.presentation.rmi.StubAdapter.request;
+import java.sql.*;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -15,12 +17,46 @@ import java.util.ArrayList;
  * @author Filip
  */
 public class DeliveryDb extends Database{
-    
 
+    private static final String SELECT_DELIVERY = "select * from Delivery where (student_id, module_id) = (?,?)";
+    private static final String SELECT_DELIVERY_WITH_USER_NAME = "select u.user_name, d.delivery_id, d.delivery_timestamp, d.student_id, d.module_id,  d.worklist_id, d.delivery_content from Delivery d inner join Users u on d.student_id = u.user_id where d.delivery_id = ?";
     private static final String ADD_DELIVERY = "insert into Delivery values (default, ?, ?, ?, ?, default, default)";
     private static final String GET_DELIVERY_FORM ="select * from Module where module_id = ?";
     private static final String SLCT_ALL_DELIVERIES = "select * from Delivery where module_id = ?";
-        
+    
+    public DeliveryDb() {
+        init();
+    }
+    
+    public Delivery getDeliveryWithUser(String deliveryId) {
+        try (
+                Connection conn = getConnection();
+                PreparedStatement ps = conn.prepareStatement(SELECT_DELIVERY_WITH_USER_NAME);
+                ) {
+            ps.setString(1, deliveryId);
+            try (
+                    ResultSet rs = ps.executeQuery();
+                    ) {
+                rs.next();
+         
+                Delivery delivery = new Delivery();
+                
+                delivery.setDeliveryID(rs.getInt("delivery_id"));
+                delivery.setStudentID(rs.getString("student_id"));
+                delivery.setStudentName(rs.getString("user_name"));
+                delivery.setModuleID(rs.getString("module_id"));
+                delivery.setDeliveryContent(rs.getString("delivery_content"));
+                delivery.setWorklistID(rs.getInt("worklist_id"));
+                delivery.setDeliveryTimestamp(rs.getString("delivery_timestamp"));
+                
+                return delivery;
+            }
+        } catch (SQLException ex) {
+            System.out.println("Method: getDeliveryWithUser(), error: " + ex);
+            return null;
+        }
+    }
+   
         public void getNrOfDeliveries(String module_id,PrintWriter out){
            try (
                 Connection conn = getConnection();
@@ -76,6 +112,7 @@ public class DeliveryDb extends Database{
         catch (SQLException ex) {
             System.out.println("Query error:" + ex);
         }
+    }
     public ArrayList<Delivery> getDelivery(PrintWriter out, String studentID) {
         
         String deliveries = "select d.delivery_id, d.delivery_content, d.delivery_timestamp, d.delivery_isEvaluated, w.worklist_id, s.user_id, m.module_id, m.module_name\n" +
@@ -122,9 +159,10 @@ public class DeliveryDb extends Database{
 
     }
        
+
         return null;
     
-
+    }
     public void addDelivery(String studentid, String moduleid, String content, String worklistid) {
         try( Connection conn = getConnection();
              PreparedStatement ps = conn.prepareStatement(ADD_DELIVERY);
@@ -141,6 +179,79 @@ public class DeliveryDb extends Database{
             System.out.println(ex);
         }
     }
+    
+    public boolean editModule (String moduleId, String content){
+        String moduleId = request.getParameter("id");
+        String editDeliveryContent = "update Delivery set delivery_content = ?, where delivery_id = ?";
+        
+        try(
+            Connection connection = getConnection();
+            PreparedStatement prepStatement = connection.prepareStatement(editDeliveryContent);
+        
+        ) {
+            prepStatement.setString(1, content);
+            prepStatement.setString(1, content);
+        
+            prepStatement.executeUpdate();
+            
+            
+            return true;
+          }
+        
+        catch(SQLException ex)  {
+            out.println("Error");
+        }
+        return false;
+    }
+    
+    //VVVV FOSSE VVVVV
+
+    public int getAllEvaluatedDeliveries(PrintWriter out) {
+        String oneDelivery = ("select * from Delivery where delivery_isEvaluated = 1;");
+        int allEvaluatedDeliveriesCount = 0;
+        
+        try(    Connection connection = getConnection();
+                PreparedStatement prepStatement = connection.prepareStatement(oneDelivery);
+                ){
+            
+            try(ResultSet rset = prepStatement.executeQuery(); ){
+                while(rset.next())   {
+                    allEvaluatedDeliveriesCount++;
+                }  
+                return allEvaluatedDeliveriesCount;
+            }
+        }
+        catch(SQLException liste) {
+            out.println("SQL exception: in getAllEvaluatedDeliveries" + liste);
+        }  
+        return allEvaluatedDeliveriesCount;
+    }     
+    
+    
+    public int getEvaluatedDeliveries(PrintWriter out, String id) {
+        String oneDelivery = ("select * from Delivery where student_id = ? AND delivery_isEvaluated = 1;");
+        int evaluatedDeliveriesCount = 0;
+        
+        try(    Connection connection = getConnection();
+                PreparedStatement prepStatement = connection.prepareStatement(oneDelivery);
+                ){
+            prepStatement.setString(1, id);
+            
+            try(ResultSet rset = prepStatement.executeQuery(); ){
+                while(rset.next())   {
+                    evaluatedDeliveriesCount++;
+                }  
+                return evaluatedDeliveriesCount;
+            }
+        }
+        catch(SQLException liste) {
+            out.println("SQL exception: in getEvaluatedDeliveries" + liste);
+        }  
+        return evaluatedDeliveriesCount;
+    }     
+    //^^^^ FOSSE ^^^^
+    
+    
 }
  
      
