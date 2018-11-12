2d7e1351a2415e1d7cefc38987fba152353e6c5f
Vegard Alvsaker <vegaal17@student.uia.no>
2018-11-12 17:53:57 +0100

    Lagde klasser for inbox

---
+++ LearningEnhancementSystem/src/java/Classes/Message.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+import java.sql.Timestamp;
+/**
+ *
+ * @author Vegard
+ */
+public class Message {
+    private String msgId;
+    private String sender;
+    private String receiver;
+    private String subject;
+    private String text;
+    private Timestamp timestamp;
+    private boolean read;
+
+    public String getMsgId() {
+        return msgId;
+    }
+
+    public void setMsgId(String msgId) {
+        this.msgId = msgId;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public void setRead(boolean read) {
+        this.read = read;
+    }
+    
+    
+}
---
+++ LearningEnhancementSystem/src/java/Database/InboxDb.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Database;
+
+import java.util.ArrayList;
+import Classes.Message;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+/**
+ *
+ * @author Vegard
+ */
+public class InboxDb extends Database {
+    
+    private static final String SELECT_ALL_MESSAGES = "select * from Message where user_id = ?";
+    
+    public InboxDb() {
+        init();
+    }
+    
+    public ArrayList<Message> getUsersMessages(String userId) {
+        ArrayList<Message> messages = new ArrayList<>();
+        try (
+                Connection conn = getConnection();
+                PreparedStatement ps = conn.prepareStatement(SELECT_ALL_MESSAGES);)
+                 {
+            ps.setString(1, userId);
+            try (ResultSet rs = ps.executeQuery();) {
+                while (rs.next()) {
+                    Message message = new Message();
+                    message.setMsgId(rs.getString("msg_id"));
+                    message.setSubject(rs.getString("msg_subject"));
+                    message.setSender(rs.getString("msg_sender"));
+                    message.setReceiver(rs.getString("msg_receiver"));
+                    message.setText(rs.getString("msg_text"));
+                    message.setTimestamp(rs.getTimestamp("msg_timestamp"));
+                    message.setRead(rs.getBoolean("msg_read"));
+                    messages.add(message);
+                }
+                return messages;
+            }
+        } catch (SQLException ex) {
+                System.out.println("Method: getUsersMessages(), Error: " + ex);
+                return null;
+                }  
+    }
+}
