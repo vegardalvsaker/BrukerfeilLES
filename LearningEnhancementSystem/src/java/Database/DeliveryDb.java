0eb4e0d88be4cd6dd18fbdc55e5675b2530d08ba
Vegard Alvsaker <35599899+vegardalvsaker@users.noreply.github.com>
2018-11-16 11:57:02 +0100

    Update DeliveryDb.java

--- LearningEnhancementSystem/src/java/Database/DeliveryDb.java
+++ LearningEnhancementSystem/src/java/Database/DeliveryDb.java
@@ -27,6 +27,35 @@ public class DeliveryDb extends Database{
     private static final String EDIT_DELIVERY = "select delivery_id, delivery content from Delivery";
     private static final String UPDATE_DELIVERY ="update Delivery set delivery_content = ? where delivery_id = ?";
     
+     public ArrayList<Delivery> getUnevaluatedDeliveriesWithinWorklist(String worklistId) {
+        ArrayList<Delivery> deliveries = new ArrayList<>();
+        try (
+                Connection conn = getConnection();
+                PreparedStatement ps = conn.prepareStatement(SELECT_ALL_DELIVERIES)) {
+            ps.setString(1, worklistId);
+            try (ResultSet rset = ps.executeQuery();) {
+                while (rset.next()) {
+                    String delId = rset.getString("delivery_id");
+                    String studentId = rset.getString("student_id");
+                    String moduleId = rset.getString("module_id");
+                    String desc = rset.getString("delivery_content");
+                    String workId = rset.getString("worklist_id");
+                    String timestamp = rset.getString("delivery_timestamp");
+                    boolean isEvaluated = rset.getBoolean("delivery_isEvaluated");
+                                        
+                    Delivery del = new Delivery(delId, studentId, moduleId, desc, workId, timestamp, isEvaluated);
+                    deliveries.add(del);
+
+                }
+                return deliveries;
+            }
+        } catch (SQLException ex) {
+            System.out.print("Method: getUnevaluatedDeliveriesWithinWorklist(), error:" + ex);
+            return null;
+        }
+        
+    }
+    
     public List<Delivery> getDeliveryWithUserIdAndModuleId(String moduleId, String studentId) {
         ArrayList<Delivery> delivery =new ArrayList<>();
         try (
